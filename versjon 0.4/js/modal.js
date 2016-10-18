90593532662b143f25c0679b74b7f6213f90c282
Pontus Ankarberg <pontus.a.ankarberg@hiof.no>
2016-10-18 21:53:30 +0200

    tissepause
    forside ferdig(ish), alle modal vinduer oppretta (gjenstår å endre
    innhold, + andre ting sikkert som jeg har glemt

--- versjon 0.4/js/modal.js
+++ versjon 0.4/js/modal.js
@@ -6,6 +6,16 @@ window.onload = function() {
     /* Hent knappene for hvert album inn i varabler */
     var btn1 = document.getElementById("myBtn1");
     var btn2 = document.getElementById("myBtn2");
+    var btn3 = document.getElementById("myBtn3");
+    var btn4 = document.getElementById("myBtn4");
+    var btn5 = document.getElementById("myBtn5");
+    var btn6 = document.getElementById("myBtn6");
+    var btn7 = document.getElementById("myBtn7");
+    var btn8 = document.getElementById("myBtn8");
+    var btn9 = document.getElementById("myBtn9");
+    var btn10 = document.getElementById("myBtn10");
+    var btn11 = document.getElementById("myBtn11");
+    var btn12 = document.getElementById("myBtn12");
 
     /* Hent lukk knappen inn i en variabel */
     var lukk = document.getElementsByClassName("close")[0];
@@ -14,17 +24,90 @@ window.onload = function() {
     /* Knapp 1 */
     btn1.onclick = function() {
             modal.style.display = "block";
-            document.getElementById("modalHead").innerHTML = "Blank Banshee - MEGA"
-            document.getElementById("modalT1").innerHTML = "Rundt millenniumskiftet ble Thomas Bangalter og Guy-Manuel de Homem-Christo kongene av klubbmusikk med albumene Homework (1997) og Discovery (2001). Deretter fulgte nok en fullengder før oppmerksomheten ble rettet mot andre prosjekter, som filmmusikken til Tron: Legacy. Det skulle gå hele åtte år etter Human After All (2005), men her er Daft Punk endelig tilbake med sitt fjerde studioalbum: Random Access Memories er en varm, oppfinnsom og oppriktig hyllest til dansemusikken. Første single fra albumet var Get Lucky, en tidløs discodiamant med lekker Pharrell-falsett og genialt funky gitarspill av Nile Rodgers. Blant de øvrige gjestene på albumet finner vi Paul Williams, legenden Giorgio Moroder, Todd Edwards, Panda Bear og The Strokes-frontmann Julian Casablancas."
-            document.getElementById("modalT2").innerHTML = "Sporliste: [1. Give Life Back To Music] [2. The Game Of Love] [3. Giorgio By Moroder] [4. Within] [5. Instant Crush (featuring Julian Casablancas) ] [6.Lose Yourself To Dance (featuring Pharrell Williams)] [7.Touch (featuring Paul Williams)] [8. Get Lucky (featuring Pharrell Williams)] [9. Beyond] [10. Motherboard] [11. Fragments Of Time (featuring Todd Edwards)] [12. Doin´ It Right (featuring Panda Bear)] [13. Contact]"
+            document.getElementById("modalHead").innerHTML = "Blank Banshee - MEGA";
+            document.getElementById("modalT1").innerHTML = "Rundt millenniumskiftet ble Thomas Bangalter og Guy-Manuel de Homem-Christo kongene av klubbmusikk med albumene Homework (1997) og Discovery (2001). Deretter fulgte nok en fullengder før oppmerksomheten ble rettet mot andre prosjekter, som filmmusikken til Tron: Legacy. Det skulle gå hele åtte år etter Human After All (2005), men her er Daft Punk endelig tilbake med sitt fjerde studioalbum: Random Access Memories er en varm, oppfinnsom og oppriktig hyllest til dansemusikken. Første single fra albumet var Get Lucky, en tidløs discodiamant med lekker Pharrell-falsett og genialt funky gitarspill av Nile Rodgers. Blant de øvrige gjestene på albumet finner vi Paul Williams, legenden Giorgio Moroder, Todd Edwards, Panda Bear og The Strokes-frontmann Julian Casablancas.";
+            document.getElementById("modalT2").innerHTML = "Sporliste: [1. Give Life Back To Music] [2. The Game Of Love] [3. Giorgio By Moroder] [4. Within] [5. Instant Crush (featuring Julian Casablancas) ] [6.Lose Yourself To Dance (featuring Pharrell Williams)] [7.Touch (featuring Paul Williams)] [8. Get Lucky (featuring Pharrell Williams)] [9. Beyond] [10. Motherboard] [11. Fragments Of Time (featuring Todd Edwards)] [12. Doin´ It Right (featuring Panda Bear)] [13. Contact]";
+            document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
         }
         /* Knapp 2 */
-        /*  btn2.onclick = function() {
-              modal.style.display = "block";
-              document.getElementById("modalHead").innerHTML = "TEST2"
-              document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst"
-              document.getElementById("modalT2").innerHTML = "teksten min er fin og ren"
-          } */
+    btn2.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+
+    btn3.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn4.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn5.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn6.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn7.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn8.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn9.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn10.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn11.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
+    btn12.onclick = function() {
+        modal.style.display = "block";
+        document.getElementById("modalHead").innerHTML = "TEST2";
+        document.getElementById("modalT1").innerHTML = "Dette er en test2 tekst";
+        document.getElementById("modalT2").innerHTML = "teksten min er fin og ren";
+        document.getElementById("modalPic").innerHTML = "<img src=\"img/album/blankbanshee-mega.jpg\" alt=\"Blank Banshee - MEGA\">";
+    }
 
     /* Når lukk knappen trykkes inn skal modal sin display stil endres */
     lukk.onclick = function() {
